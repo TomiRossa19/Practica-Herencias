753d9f5bff57c3d604af62c8049d6d3519040553
Tomás <tomasdiegorossa@gmail.com>
2024-05-07 21:06:47 -0300

    7/5 4

--- WireTech1/WireTech/WireTechService/Lógica.cs
+++ WireTech1/WireTech/WireTechService/Lógica.cs
@@ -4,20 +4,32 @@
     {
 
         public int Tipo { get; set; }
+        public int MyProperty { get; set; }
         public List<Pago> listapagos { get; set; }
 
-
+        public Pago pagos = new Pago();
 
         public void CargarEntidades(int Tipo)
         {
-            while (Tipo != 1) { 
+            while (Tipo != 1) {
+                
+               
             switch (Tipo)
                 {
                     case 2:
-                        while (Tipo == 1)
-                        {
 
-                        }
+                        
+
+                        int codpago = codpago + 1;
+                        DateTime fechapago = DateTime.Now;
+                        Console.WriteLine("Ingrese su DNI: ");
+                        int DNI = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese su código de servicio: ");
+                        int codserv = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese su importe: ");
+                        int imp = int.Parse(Console.ReadLine());
+
+                        pagos.CargarPago(codpago, fechapago, DNI, codserv, imp);
                         return;
                         
 
--- WireTech1/WireTech/WireTechService/Pago.cs
+++ WireTech1/WireTech/WireTechService/Pago.cs
@@ -6,21 +6,25 @@ using System.Threading.Tasks;
 
 namespace WireTechService
 {
-    public class Pago
+    public class Pago : Logica
     {
         public int NumPag { get; set; }
         public DateTime FechaCobro { get; set; }
         public int DNI { get; set; }
 
         public int CodServ { get; set; }
-        public void CargarPago(int NumPag, DateTime FechaCobro, int DNI, int CodServ)
+
+        public int Imp { get; set; }
+        public void CargarPago(int NumPag, DateTime FechaCobro, int DNI, int CodServ, int Imp)
         {
-            Pago pagos = new Pago();
+            
             pagos.NumPag = NumPag;
             pagos.FechaCobro = FechaCobro;
             pagos.DNI = DNI;
             pagos.CodServ = CodServ;
+            pagos.Imp = Imp;
 
+            listapagos.Add(pagos);
         }
     }
 }
